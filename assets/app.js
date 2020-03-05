ad448a1735a4c5fe258bf10beb8df8e3ce48249a
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-03-04 23:10:02 -0600

    added css and html framework; created basic functionality with click toggling via counters

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 // Create subject array to hold pre-selected button subjects
-var subjectArray = ["puppies", "kittens", "birds"];
+var subjectArray = ["alpacas", "dogs", "cats", "magic", "potatoes", "hogwarts"];
 
 // Create variables for API parameters
 
@@ -7,7 +7,8 @@ var subjectArray = ["puppies", "kittens", "birds"];
 const apiKey = "4xW69sv2q24wSAau2sHmhQGN7YLyBlWi";
 var limit = "10";
 var rating = "g";
-var subject = "magic";
+var subject = "quokka";
+var numberOfClicks = 0;
 
 // Create query URL
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + subject + "&" + limit + "=10&offset=0&rating=" + rating + "&lang=en";
@@ -15,34 +16,54 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" 
 $.ajax({
   url: queryURL,
   method: "GET"
-}).then(function(response) {
+}).then(function (response) {
   console.log("got data: " + queryURL);
 
-  // Here we define our gif results. We're working with the first ten.
-  gifResults = [
-    response.data[0].images.downsized.url,
-    response.data[2].images.downsized.url,
-    response.data[3].images.downsized.url,
-    response.data[4].images.downsized.url,
-    response.data[5].images.downsized.url,
-    response.data[6].images.downsized.url,
-    response.data[7].images.downsized.url,
-    response.data[8].images.downsized.url,
-    response.data[9].images.downsized.url,
-  ];
+  // Here we define our gif results. We're working with the first ten (i.e., indicies 0 - 9).
+  var gifResults = [
+    response.data[0].images.downsized_still.url,
+    response.data[1].images.downsized_still.url,
+    response.data[2].images.downsized_still.url,
+    response.data[3].images.downsized_still.url,
+    response.data[4].images.downsized_still.url,
+    response.data[5].images.downsized_still.url,
+    response.data[6].images.downsized_still.url,
+    response.data[7].images.downsized_still.url,
+    response.data[8].images.downsized_still.url,
+    response.data[9].images.downsized_still.url,
+  ]
 
-// Loop through the results to display them on the page
+  // Loop through the results to display them on the page
   for (var j = 0; j < gifResults.length; j++) {
 
     // Each result will be saved as an img element
-    gif = "<img src=" + response.data[j].images.downsized.url + " class= 'captured-gif' />";
+    gif = "<img src=" + response.data[j].images.downsized_still.url + " class= 'captured-gif' />";
 
     // We append the img element to the parent div
     $(".gif-div").append(gif);
   }
 
-// If there is an error getting the data, we'll console log the error message
-}).catch(function(err) {
+
+  // Found how to capture clicked element index from stackoverflow:
+  // Source: https://stackoverflow.com/questions/5545283/get-index-of-clicked-element-in-collection-with-jquery
+  $(".captured-gif").on("click", function () {
+
+    
+    numberOfClicks++;
+
+    console.log(numberOfClicks);
+
+    if (numberOfClicks % 2 === 0) {
+      ndexOfClickedGif = $(".captured-gif").index(this);
+      $(this).attr("src", response.data[indexOfClickedGif].images.downsized_still.url)
+    } else {
+      indexOfClickedGif = $(".captured-gif").index(this);
+      $(this).attr("src", response.data[indexOfClickedGif].images.downsized.url);
+    }
+  })
+
+  // If there is an error getting the data, we'll console log the error message
+}).catch(function (err) {
   console.log(err.message);
 });
 
@@ -63,7 +84,7 @@ function createButtons() {
 }
 
 // Here we'll add buttons if the user enters a search parameter
-$("#search-parameters").on("click", function(event) {
+$("#search-parameters").on("click", function (event) {
   event.preventDefault();
   var subject = $("#search-input").val().trim();
   subjectArray.push(subject);
