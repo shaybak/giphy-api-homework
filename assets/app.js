9650b428ac7298760625c7d06a6aa9656bc66e02
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-03-07 21:36:40 -0600

    all basic functionality complete

--- assets/app.js
+++ assets/app.js
@@ -6,89 +6,157 @@ var subjectArray = ["alpacas", "dogs", "cats", "magic", "potatoes", "hogwarts"];
 // apiKey is set to "constant" because we don't want it to change
 const apiKey = "4xW69sv2q24wSAau2sHmhQGN7YLyBlWi";
 var limit = "10";
-var rating = "g";
+// var rating = "g";
 var subject = "quokka";
 var numberOfClicks = 0;
+var gifStill = [];
+var gifAnimated = [];
 
-// Create query URL
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + subject + "&" + limit + "=10&offset=0&rating=" + rating + "&lang=en";
+createButtons();
+getAPIData();
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function (response) {
-  console.log("got data: " + queryURL);
+function getAPIData() {
+  // Create query URL
+  // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + subject + "&" + limit + "=10&offset=0&rating=" + rating + "&lang=en";
 
-  // Here we define our gif results. We're working with the first ten (i.e., indicies 0 - 9).
-  var gifResults = [
-    response.data[0].images.downsized_still.url,
-    response.data[1].images.downsized_still.url,
-    response.data[2].images.downsized_still.url,
-    response.data[3].images.downsized_still.url,
-    response.data[4].images.downsized_still.url,
-    response.data[5].images.downsized_still.url,
-    response.data[6].images.downsized_still.url,
-    response.data[7].images.downsized_still.url,
-    response.data[8].images.downsized_still.url,
-    response.data[9].images.downsized_still.url,
-  ]
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + subject + "&limit=" + limit + "&offset=0&lang=en&random_id:e826c9fc5c929e0d6c6d423841a282aa";
 
-  // Loop through the results to display them on the page
-  for (var j = 0; j < gifResults.length; j++) {
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-    // Each result will be saved as an img element
-    gif = "<img src=" + response.data[j].images.downsized_still.url + " class= 'captured-gif' />";
+    console.log("got data from: " + queryURL);
 
-    // We append the img element to the parent div
-    $(".gif-div").append(gif);
-  }
+    // Here we define our gif results. We're working with the first ten (i.e., indicies 0 - 9).
 
+    // array of still URLs
+    gifStill = [
+      response.data[0].images.downsized_still.url,
+      response.data[1].images.downsized_still.url,
+      response.data[2].images.downsized_still.url,
+      response.data[3].images.downsized_still.url,
+      response.data[4].images.downsized_still.url,
+      response.data[5].images.downsized_still.url,
+      response.data[6].images.downsized_still.url,
+      response.data[7].images.downsized_still.url,
+      response.data[8].images.downsized_still.url,
+      response.data[9].images.downsized_still.url,
+    ]
 
-  // Found how to capture clicked element index from stackoverflow:
-  // Source: https://stackoverflow.com/questions/5545283/get-index-of-clicked-element-in-collection-with-jquery
-  $(".captured-gif").on("click", function () {
+    // array of animated URLs
+    gifAnimated = [
+      response.data[0].images.downsized.url,
+      response.data[1].images.downsized.url,
+      response.data[2].images.downsized.url,
+      response.data[3].images.downsized.url,
+      response.data[4].images.downsized.url,
+      response.data[5].images.downsized.url,
+      response.data[6].images.downsized.url,
+      response.data[7].images.downsized.url,
+      response.data[8].images.downsized.url,
+      response.data[9].images.downsized.url,
+    ]
 
-    
-    numberOfClicks++;
+    // Loop through the results to display them on the page
+    for (var j = 0; j < gifStill.length; j++) {
 
-    console.log(numberOfClicks);
+      
 
-    if (numberOfClicks % 2 === 0) {
-      ndexOfClickedGif = $(".captured-gif").index(this);
-      $(this).attr("src", response.data[indexOfClickedGif].images.downsized_still.url)
-    } else {
-      indexOfClickedGif = $(".captured-gif").index(this);
-      $(this).attr("src", response.data[indexOfClickedGif].images.downsized.url);
+      // Each result will be saved as an img element
+      var gif = "<img src=" + gifStill[j] + " class= 'captured-gif' />";
+
+      //Create sub-div for image and rating
+      subDiv = $("<div>").addClass("img-and-rating");
+
+      // Create element for rating
+      var ratingElement = $("<p>").addClass("rating")
+
+      // Create variable rating to store rating value
+      var rating = "Rating: " + response.data[0].rating
+
+      // Append rating value to the rating element
+      ratingElement.append(rating);
+
+      // Append gif and rating element to subDiv
+      subDiv.append(gif);
+      subDiv.append(ratingElement);
+
+      // We append the subDiv  to the parent div
+      $(".gif-div").append(subDiv);
     }
-  })
-
-  // If there is an error getting the data, we'll console log the error message
-}).catch(function (err) {
-  console.log(err.message);
-});
 
 
-// Here we'll create buttons for our preselected subjects
-function createButtons() {
 
 
-  $("#buttons-div").empty();
+    // Found how to capture clicked element index from stackoverflow:
+    // Source: https://stackoverflow.com/questions/5545283/get-index-of-clicked-element-in-collection-with-jquery
+    $(".captured-gif").on("click", function () {
 
-  for (var i = 0; i < subjectArray.length; i++) {
-    var newButton = $("<button>");
-    newButton.addClass("btn btn-outline-primary btn-lg gif-button");
-    newButton.attr("data-name", subjectArray[i]);
-    newButton.text(subjectArray[i]);
-    $("#buttons-div").append(newButton);
-  }
+      var indexOfClickedGif = "";
+
+      numberOfClicks++;
+
+      console.log(numberOfClicks);
+
+      if (numberOfClicks % 2 === 0) {
+        indexOfClickedGif = $(".captured-gif").index(this);
+        $(this).attr("src", gifStill[indexOfClickedGif]);
+      } else {
+        indexOfClickedGif = $(".captured-gif").index(this);
+        $(this).attr("src", gifAnimated[indexOfClickedGif]);
+      }
+    });
+
+    // If there is an error getting the data, we'll console log the error message
+  }).catch(function (err) {
+    console.log(err.message);
+  });
+
 }
 
 // Here we'll add buttons if the user enters a search parameter
 $("#search-parameters").on("click", function (event) {
   event.preventDefault();
-  var subject = $("#search-input").val().trim();
-  subjectArray.push(subject);
+  var newSubject = $("#search-input").val().trim();
+  subjectArray.push(newSubject);
+
+  console.log(subjectArray);
+  console.log(newSubject);
+
+  subject = newSubject;
+  $(".gif-div").empty();
+  $("#search-input").val("");
+  getAPIData(subject);
   createButtons();
 });
 
-createButtons();
+// $(".subject-button").on("click", function () {
+//   subject = $(this).attr("data-name");
+//   // console.log(subject);
+//   $(".gif-div").empty();
+//   getAPIData(subject);
+// });
+
+$(document).on("click", ".subject-button", function () {
+  subject = $(this).attr("data-name");
+  // console.log(subject);
+  $(".gif-div").empty();
+  getAPIData(subject);
+})
+
+// Here we'll create buttons for our preselected subjects
+function createButtons() {
+
+  $("#buttons-div").empty();
+
+  for (var i = 0; i < subjectArray.length; i++) {
+    var newButton = $("<button>");
+    newButton.addClass("btn btn-outline-primary btn-lg subject-button");
+    newButton.attr("data-name", subjectArray[i]);
+    newButton.text(subjectArray[i]);
+    $("#buttons-div").append(newButton);
+
+  }
+}
